0ebb1cccf8f5a2832418183ad65e8621f9cff127
Luan Rafael <luanraffaell@hotmail.com>
2025-04-23 07:19:01 -0300

    chore: react hot toast

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,15 @@ import './index.css'
 import { RouterProvider } from 'react-router'
 import { router } from './App'
 import CartProvider from './context/cartContext'
+import { Toaster } from 'react-hot-toast'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <CartProvider>
+      <Toaster
+        position='top-center'
+        reverseOrder={false}
+      />
     <RouterProvider router={router}/>
     </CartProvider>
   </StrictMode>,
